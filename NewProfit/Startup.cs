1f78a5f0129c0ff2a38014e2aefc9a3f9e8fc461
ddt626 <eightddt@gmail.com>
2014-09-29 23:48:22 +0800

    NewProfit first version

---
+++ NewProfit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewProfit.Startup))]
+namespace NewProfit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
